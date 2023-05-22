cb1c4d5bac89fd9e2a4b6bc70afb108f436ee534
tanishkassingh <tanishkasingh8888@gmail.com>
2023-05-22 23:54:36 +0530

    handled cart and checkoutpage for logged in users

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -143,6 +143,7 @@ MESSAGE_TAGS={
 
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
-EMAIL_HOST_USER='shivay6e@gmail.com'
-EMAIL_HOST_PASSWORD='jmbgdszpjuofypiq'
+EMAIL_HOST_USER='tanishkaaasingh@gmail.com'
+EMAIL_HOST_PASSWORD='vakuuesmbtdbmovp'
 EMAIL_USE_TLS=True
+
